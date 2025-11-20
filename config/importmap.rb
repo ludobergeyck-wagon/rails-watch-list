ea731ef8838a5ef4741a87f3db7d42d33145d499
Ludo de Bergeyck <ludo.bergeyck@gmail.com>
2025-11-20 11:23:12 +0100

    config-watch-list

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,5 @@
-# Pin npm packages by running ./bin/importmap
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true# Pin npm packages by running ./bin/importmap
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
